afcc3cd2876f748c0305fa494ba63a5e3afa3b74
ᴀʟᴇᴊᴏ ᴀᴄᴇᴠᴇᴅᴏ <129635833+Alejoacs@users.noreply.github.com>
2024-08-24 12:46:33 -0500

    Stories | 24/08/2024 12:45pm

--- src/routes/user/story.routes.js
+++ src/routes/user/story.routes.js
@@ -3,12 +3,12 @@ import { getAllStories, getStoryById, getAllMyStories, createStory, deleteStory,
 import { verifyToken, isUser } from '../../middlewares/authJwt.js';
 const router = express.Router();
 
-router.post('/', [verifyToken, isUser], createStory);
-router.delete('/:storyId', [verifyToken, isUser], deleteStory);
-router.get('/my', [verifyToken, isUser], getAllMyStories);
-router.get('/', [verifyToken, isUser], getAllStories);
-router.get('/:storyId', [verifyToken, isUser], getStoryById);
-router.post('/:storyId/view', [verifyToken, isUser], viewStory);
-router.get('/:storyId/view', [verifyToken, isUser], getViewStory);
+router.post('/', [verifyToken], createStory);
+router.delete('/:storyId', [verifyToken], deleteStory);
+router.get('/my', [verifyToken], getAllMyStories);
+router.get('/', [verifyToken], getAllStories);
+router.get('/:storyId', [verifyToken], getStoryById);
+router.post('/:storyId/view', [verifyToken], viewStory);
+router.get('/:storyId/view', [verifyToken], getViewStory);
 
 export default router;
--- src/routes/user/user.routes.js
+++ src/routes/user/user.routes.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import {getAllUsers, getFollowers, getFollowing, getFriends, getUsersById, getUsersByUsername, searchUsers, deleteUser, updateStatus, updateUser, followUser, unfollowUser, addFriend, removeFriend, changeRole } from '../../controllers/User/user.controller.js';
+import { getAllUsers, getFollowers, getFollowing, getFriends, getUsersById, getUsersByUsername, searchUsers, deleteUser, updateStatus, updateUser, followUser, unfollowUser, addFriend, removeFriend, changeRole } from '../../controllers/User/user.controller.js';
 import { verifyToken, isUser, isAdmin, isModeratorOrAdmin } from '../../middlewares/authJwt.js';
 
 const router = express.Router();
@@ -17,13 +17,13 @@ router.put('/status/:userId', [verifyToken, isModeratorOrAdmin], updateStatus);
 
 // User
 router.get('/:userId', [verifyToken], getUsersById);
-router.post('/follow/:userId', [verifyToken, isUser], followUser)
-router.post('/unfollow/:userId', [verifyToken, isUser], unfollowUser)
-router.get('/followers/:userId', [verifyToken, isUser], getFollowers)
-router.get('/following/:userId', [verifyToken, isUser], getFollowing)
-router.post('/addFriend/:userId', [verifyToken, isUser], addFriend)
-router.post('/removeFriend/:userId', [verifyToken, isUser], removeFriend)
-router.get('/friends/:userId', [verifyToken, isUser], getFriends)
-router.post('/search', [verifyToken, isUser], searchUsers)
+router.post('/follow/:userId', [verifyToken], followUser)
+router.post('/unfollow/:userId', [verifyToken], unfollowUser)
+router.get('/followers/:userId', [verifyToken], getFollowers)
+router.get('/following/:userId', [verifyToken], getFollowing)
+router.post('/addFriend/:userId', [verifyToken], addFriend)
+router.post('/removeFriend/:userId', [verifyToken], removeFriend)
+router.get('/friends/:userId', [verifyToken], getFriends)
+router.post('/search', [verifyToken], searchUsers)
 
 export default router;
